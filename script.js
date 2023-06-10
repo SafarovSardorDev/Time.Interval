e5a4dc608538e4c65c3a413783d21b1c0a62c4b4
SafarovSardorDev <sardor.safarov200320@gmail.com>
2023-06-10 16:51:26 +0500

    create pop up with hand

--- script.js
+++ script.js
@@ -1,7 +1,39 @@
 'use strict'
 
 
+// const box = document.querySelector('.box');
+// setTimeout(() => {
+//     box.style.backgroundColor = 'black'
+// }, 2000);
+
+
+// const box = document.querySelector('.box');
+// let counter = 0;
+// const countInterval = setInterval(count, 1000);
+
+// function count() {
+//     console.log(counter++);
+//     if(counter > 5) {
+//         clearInterval(countInterval)
+//     }
+//     if(counter > 5) {
+//         box.style.marginLeft = 'auto'
+//     }
+// }
+
+
+const span = document.querySelector('span')
 const box = document.querySelector('.box');
+const body = document.querySelector('body')
+
 setTimeout(() => {
-    box.style.backgroundColor = 'black'
-}, 2000);
+    box.style.display = 'block'
+}, 5000);
+
+span.addEventListener('click', () => {
+    box.style.display = 'none'
+})
+
+body.addEventListener('click', () => {
+        clearTimeout()
+})
